e6568a78210c57df5856178dd7c9f752f2cf0b3d
artemave <artemave@gmail.com>
2011-10-15 20:39:33 +0100

    RestAssured::Server#start spec wip

--- features/step_definitions/ruby_api_steps.rb
+++ features/step_definitions/ruby_api_steps.rb
@@ -2,18 +2,17 @@ Given /^rest\-assured server is not running$/ do
 end
 
 When /^I start rest\-assured server via client library$/ do
-  @server = RestAssured::Server.new
-  @server.start
+  @server_proc = RestAssured::Server.start
 end
 
 Then /^rest\-assured server should be running$/ do
   60.times do
-    if `ps a | grep rest-assured`[/#{@server.port}/]
+    if `ps a | grep rest-assured`[/#{@server_proc.psname}/]
       break
     else
       sleep 1
     end
   end
 
-  `ps a | grep rest-assured`[/#{@server.port}/].should_not be_nil
+  `ps a | grep rest-assured`[/#{@server_proc.psname}/].should_not be_nil
 end
